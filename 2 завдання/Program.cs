7a5785e005a208f0bd233329f8f766bd9457df58
sarik <sarik@ANDRII>
2024-04-08 15:19:41 +0200

    Add project files.

---
+++ 2 завдання/Program.cs
@@ -0,0 +1,22 @@
+﻿//The discount
+
+
+double price = 300;
+double percent = 20;
+
+double increse = price * percent;
+double divide = increse / 100;
+
+Console.WriteLine($"The discount is:{divide}");
+
+//Time in seconds
+
+double hours = 7;
+double minutes = 50;
+double seconds = 55;
+
+double calculationOfHours = hours * 3600;
+double  calculationOfMinutes = minutes * 60;
+
+double calculationOfSeconds = calculationOfHours + calculationOfMinutes + seconds;
+Console.WriteLine($"The seconds in time which you wrote: {calculationOfSeconds}");
